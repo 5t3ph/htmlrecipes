4da83eba37fc0df5e0ad74b26b7b80ea84c76040
5t3ph <thinkdobecreate@gmail.com>
2021-07-24 15:27:04 -0500

    ✨ v1 Launch

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,18 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight);
+  
   eleventyConfig.addWatchTarget("./src/sass/");
+
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+
+  eleventyConfig.addCollection("orderedSnippets", function (collection) {
+    return collection.getFilteredByTag("snippets").sort((a, b) => {
+      return a.data.order - b.data.order;
+    });
+  });
 
   return {
     dir: {
